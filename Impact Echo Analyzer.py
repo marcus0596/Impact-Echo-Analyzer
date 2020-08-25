0fdc9747dae326bbebd9e54e7bd2e2ed4b5a10b4
Marcus Lowry <marcusl@bditest.com>
2020-08-25 19:55:03 -0400

    Plotter and fft functions work now.

--- Impact Echo Analyzer.py
+++ Impact Echo Analyzer.py
@@ -14,50 +14,61 @@ import os
 import time
 
 
-lvm_folder = 'C:\Users\marcusl\Downloads\Training\Impact Echo Analysis\Files Needing Categorization\lvm'
+lvm_folder = 'C:\Users\marcusl\Downloads\Training\Impact-Echo-Analyzer\Files Needing Categorization\lvm'
 delam_folder = 'C:\Users\marcusl\Downloads\Training\Impact Echo Analysis\Delamination'
 non_folder = 'C:\Users\marcusl\Downloads\Training\Impact Echo Analysis\No Delamination'
 
 start = time.time()
 
 class ImpactEcho:
-    def __init__(self,path=lvm_folder,freq_offset=200):
-        self.path = path
-        self.freq_offset = freq_offset
+    def __init__(self,PATH=lvm_folder,FREQ_OFFSET=200):
+        self.PATH = PATH
+        self.LVM_FILES = [files for files in os.listdir(self.PATH) if 
+                     files.lower().endswith('.lvm')]
+        self.FREQ_OFFSET = FREQ_OFFSET
         
         # Prepare data
-        self.data = pd.read_csv(path,skiprows=22, delimiter='\t')
+        self.data = pd.read_csv(os.path.join(self.PATH,self.LVM_FILES[0]),
+                                skiprows=22, delimiter='\t')
         self.data.rename(index={0:'X_Value', 1:'Acceleration'})
         
-        # Calculate the sampling frequency
-        self.Fs=1 / (self.data.X_Value[1])
-        
     def ffTransform(self):
+        
+        # Calculate the sampling frequency
+        Fs=1 / (self.data.X_Value[1])
+        
         n = len(self.data)  # length of the signal
         k = np.arange(n)
-        T = n / self.Fs
-        freq = k / T  # two sides frequency range
-        freq = freq[range(int(n / 2))]  # one side frequency range
+        T = n / Fs
+        self.freq = k / T  # two sides frequency range
+        self.freq = self.freq[range(int(n / 2))]  # one side frequency range
     
-        amp = np.fft.fft(self.data) / n  # fft computing and normalization
-        amp = abs(amp[range(int(n / 2))])
-        amp = amp / max(amp)
+        self.amp = np.fft.fft(self.data[['Acceleration']]) / n  # fft computing and normalization
+        self.amp = abs(self.amp[range(int(n / 2))])
+        self.amp = self.amp / max(self.amp)
     
-        return freq[self.freq_offset:], amp[self.freq_offset:]
-    
-    def Plotter(self):
+    def Plot(self):
         #Perform FFT
-        freq,amp= self.ffTransform(self.data.Acceleration, self.Fs)
+        self.ffTransform()
         
         #Plot results
         plt.subplot(2,1,1)
-        plt.plot(data.X_Value, data.Acceleration)
+        plt.plot(self.data.X_Value, self.data.Acceleration)
         plt.xlabel('Time (s)')
         plt.ylabel('Amplitude')
         plt.subplot(2,1,2)
-        plt.plot(freq[200:],amp[200:])
+        plt.plot(self.freq[200:],self.amp[200:])
         plt.xlabel('Frequency (Hz)')
         plt.ylabel('Amplitude')
         plt.show()
+        
+    def Analysis(self):
+        
+        
+        comments = []
+        pass
+
+ie = ImpactEcho()
+ie.Plot()
 
 print time.time() - start
