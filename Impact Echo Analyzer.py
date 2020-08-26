78e4ab052c398aa956f506ccf626003e29fe2564
Marcus Lowry <marcusl@bditest.com>
2020-08-25 22:03:09 -0400

    Added moveFiles and undo functionality

--- Impact Echo Analyzer.py
+++ Impact Echo Analyzer.py
@@ -11,6 +11,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import shutil
 import time
 
 
@@ -21,32 +22,84 @@ non_folder = 'C:\Users\marcusl\Downloads\Training\Impact Echo Analysis\No Delami
 start = time.time()
 
 class ImpactEcho:
-    def __init__(self,PATH=lvm_folder,FREQ_OFFSET=200):
-        self.PATH = PATH
-        self.LVM_FILES = [files for files in os.listdir(self.PATH) if 
+    def __init__(self,root=lvm_folder,freq_offset=200):
+        self.root = root
+        self.delam_folder = os.path.join(self.root,'Delaminations')
+        self.non_folder = os.path.join(self.root,'Non Delaminations')
+        self.LVM_FILES = [files for files in os.listdir(self.root) if 
                      files.lower().endswith('.lvm')]
-        self.FREQ_OFFSET = FREQ_OFFSET
+        self.FREQ_OFFSET = freq_offset
+        self.FREQ_THRESHOLD=8000
+        self.comments = []
+        
+        
+    def ffTransform(self,data):
+        
+        n = len(data)  # length of the signal
+    
+        amp = np.fft.fft(data.Acceleration) / n  # fft computing and normalization
+        amp = abs(amp[range(int(n / 2))])
+        amp = amp / max(amp)
+        
+        return amp[self.FREQ_THRESHOLD:]
+    
+    def Analysis(self,FREQ_THRESHOLD=8000):
+        start = time.time()
+        self.FREQ_THRESHOLD = FREQ_THRESHOLD
         
         # Prepare data
-        self.data = pd.read_csv(os.path.join(self.PATH,self.LVM_FILES[0]),
+        for i in range(len(self.LVM_FILES)):
+            df = pd.read_csv(os.path.join(self.root,self.LVM_FILES[i]),
                                 skiprows=22, delimiter='\t')
-        self.data.rename(index={0:'X_Value', 1:'Acceleration'})
+            df.rename(index={0:'X_Value', 1:'Acceleration'})
+            amp = self.ffTransform(df)
+            
+            if np.argmax(amp)<=self.FREQ_THRESHOLD:
+                self.comments.append([self.LVM_FILES[i],'good'])
+            else:
+                self.comments.append([self.LVM_FILES[i],'bad'])
         
-    def ffTransform(self):
+        timed = round(time.time() - start,3)
+        print 'Analysis took {timed} seconds.'.format(timed=timed)
         
-        # Calculate the sampling frequency
-        Fs=1 / (self.data.X_Value[1])
+    def moveFiles(self):
+        # Analyze data
+        self.Analysis()
         
-        n = len(self.data)  # length of the signal
-        k = np.arange(n)
-        T = n / Fs
-        self.freq = k / T  # two sides frequency range
-        self.freq = self.freq[range(int(n / 2))]  # one side frequency range
-    
-        self.amp = np.fft.fft(self.data[['Acceleration']]) / n  # fft computing and normalization
-        self.amp = abs(self.amp[range(int(n / 2))])
-        self.amp = self.amp / max(self.amp)
-    
+        # Create folders
+        try:
+            os.mkdir(self.delam_folder)
+        except:
+            pass
+        try:
+            os.mkdir(self.non_folder)
+        except:
+            pass
+        
+        for i in range(len(self.comments)):
+            src = os.path.join(self.root,self.comments[i][0])
+            if self.comments[i][1]=='good':
+                 dst = os.path.join(self.root,self.non_folder,
+                                    self.comments[i][0])
+                 shutil.move(src,dst)
+            else:
+                 dst = os.path.join(self.root,self.delam_folder,
+                                    self.comments[i][0])
+                 shutil.move(src,dst)
+                 
+    def undo(self):
+        if os.path.isdir(self.delam_folder):
+            for bad in os.listdir(self.delam_folder):
+                src = os.path.join(self.delam_folder,bad)
+                shutil.move(src,self.root)
+                
+            for good in os.listdir(self.non_folder):
+                src = os.path.join(self.non_folder,good)
+                shutil.move(src,self.root)
+                
+            os.rmdir(self.delam_folder)
+            os.rmdir(self.non_folder)
+ 
     def Plot(self):
         #Perform FFT
         self.ffTransform()
@@ -57,18 +110,13 @@ class ImpactEcho:
         plt.xlabel('Time (s)')
         plt.ylabel('Amplitude')
         plt.subplot(2,1,2)
-        plt.plot(self.freq[200:],self.amp[200:])
+        plt.plot(self.freq,self.amp)
         plt.xlabel('Frequency (Hz)')
         plt.ylabel('Amplitude')
         plt.show()
-        
-    def Analysis(self):
-        
-        
-        comments = []
-        pass
 
-ie = ImpactEcho()
-ie.Plot()
-
-print time.time() - start
+test=os.path.join(lvm_folder,'test')
+ie = ImpactEcho(test)
+ie.moveFiles()
+#ie.undo()
+#ie.Plot()
